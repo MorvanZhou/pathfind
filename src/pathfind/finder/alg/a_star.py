7b2876b0d5a596dad633dfa72f4e8c27a0b38c46
morvanzhou <morvanzhou@hotmail.com>
2022-11-15 14:07:40 +0800

    a* add two priority, the first one is g+h, the second one is g

--- src/pathfind/finder/alg/a_star.py
+++ src/pathfind/finder/alg/a_star.py
@@ -5,6 +5,8 @@ from pathfind.graph.graph import Node
 class AStar(GreedyBestFirst):
     def __init__(self, k: float = 1.):
         super().__init__()
+        if k < 0:
+            k = 0
         self.k = k
 
     def check_neighbors(self, current: Node):
@@ -12,6 +14,8 @@ class AStar(GreedyBestFirst):
             new_cost = self.cost_to(current) + cost
             if not self.is_discovered(neighbor) or new_cost < self.cost_to(neighbor):
                 self.discover(neighbor, new_cost)
-                priority = new_cost + self.k * self.heuristic(neighbor)
-                self.frontier.put(neighbor, priority)
+                priority = new_cost
+                if self.k > 0:
+                    priority += self.k * self.heuristic(neighbor)
+                self.frontier.put(neighbor, priority, new_cost)  # same f = g + h, then sort g
                 self.came_from[neighbor.name] = current
